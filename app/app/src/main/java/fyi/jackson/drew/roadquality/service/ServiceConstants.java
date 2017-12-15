03a558d03ca625e85181a927ae427a7010309608
David Jackson <jack8679@kettering.edu>
2017-12-15 16:07:14 -0500

    Added Service constants for DatabaseService

---
+++ app/app/src/main/java/fyi/jackson/drew/roadquality/service/ServiceConstants.java
@@ -0,0 +1,21 @@
+package fyi.jackson.drew.roadquality.service;
+
+
+public class ServiceConstants {
+    public static final String LONG_TERM_DATA_SERVICE_DELETED_ACCELEROMETER_ENTRIES_COUNT = "DELETED_ACCELEROMETER_ENTRIES";
+    public static final String LONG_TERM_DATA_SERVICE_DELETED_GPS_ENTRIES_COUNT = "DELETED_GPS_ENTRIES";
+    public static final String LONG_TERM_DATA_SERVICE_ROAD_POINT_ENTRIES_COUNT = "ROAD_POINT_ENTRY_COUNT";
+
+    public static final String TRIP_IDS_COUNT = "TRIP_IDS_COUNT";
+
+
+    public static final String SERVICE_PROCESS_TAG = "IntentServiceProcess";
+
+    public static final String PROCESS_NOT_DEFINED = "fyi.jackson.drew.roadquality.service.DatabaseService.PROCESS_NOT_DEFINED";
+    public static final String PROCESS_LONG_TERM_STORAGE = "fyi.jackson.drew.roadquality.service.DatabaseService.PROCESS_LONG_TERM_STORAGE";
+    public static final String PROCESS_GET_ALL_TRIP_IDS = "fyi.jackson.drew.roadquality.service.DatabaseService.PROCESS_GET_ALL_TRIP_IDS";
+    public static final String PROCESS_GET_ALL_POINTS_FROM_TRIP = "fyi.jackson.drew.roadquality.service.DatabaseService.PROCESS_GET_ALL_POINTS_FROM_TRIP";
+    public static final String PROCESS_GET_ALL_GPS_ROAD_POINTS = "fyi.jackson.drew.roadquality.service.DatabaseService.PROCESS_GET_ALL_GPS_ROAD_POINTS";
+
+
+}
