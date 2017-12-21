b2f76763b8fd03c4741e009ea16b69d18eecf1b4
David Jackson <jack8679@kettering.edu>
2017-12-21 10:06:11 -0500

    Added test activity for sandboxing new features

---
+++ app/app/src/main/java/fyi/jackson/drew/roadquality/ActivityTest.java
@@ -0,0 +1,13 @@
+package fyi.jackson.drew.roadquality;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
