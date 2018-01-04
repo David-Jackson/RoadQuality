3b41c432f17c47827c35e96d7cf9eb059a8aa1ab
David Jackson <jack8679@kettering.edu>
2018-01-04 16:23:25 -0500

    Update with abstract methods and added tripList receiver

--- app/app/src/main/java/fyi/jackson/drew/roadquality/utils/BroadcastManager.java
+++ app/app/src/main/java/fyi/jackson/drew/roadquality/utils/BroadcastManager.java
@@ -4,15 +4,23 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.util.Log;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import fyi.jackson.drew.roadquality.service.DatabaseService;
 import fyi.jackson.drew.roadquality.service.ServiceConstants;
 
 // Manager to receive broadcasts from various services
 
-public class BroadcastManager {
+public abstract class BroadcastManager {
+    private String TAG = "BroadcastManager";
 
     private BroadcastReceiver longTermDataReceiver;
+    private BroadcastReceiver tripListReceiver;
     private Context context;
 
     public BroadcastManager (Context context) {
@@ -24,10 +32,31 @@ public class BroadcastManager {
         longTermDataReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context receiveContext, Intent intent) {
-                int totalRows = intent.getIntExtra(ServiceConstants.LONG_TERM_DATA_SERVICE_ROAD_POINT_ENTRIES_COUNT, -999);
-                int deletedAccelRows = intent.getIntExtra(ServiceConstants.LONG_TERM_DATA_SERVICE_DELETED_ACCELEROMETER_ENTRIES_COUNT, -998);
-                int deletedGpsRows = intent.getIntExtra(ServiceConstants.LONG_TERM_DATA_SERVICE_DELETED_GPS_ENTRIES_COUNT, -987);
-                Toast.makeText(context, "Data Received: total = " + totalRows + " | delA = " + deletedAccelRows + " | delG =" + deletedGpsRows, Toast.LENGTH_LONG).show();
+                int totalRows = intent.getIntExtra(
+                        ServiceConstants.LONG_TERM_DATA_SERVICE_ROAD_POINT_ENTRIES_COUNT,
+                        -999);
+                int deletedAccelRows = intent.getIntExtra(
+                        ServiceConstants.LONG_TERM_DATA_SERVICE_DELETED_ACCELEROMETER_ENTRIES_COUNT,
+                        -998);
+                int deletedGpsRows = intent.getIntExtra(
+                        ServiceConstants.LONG_TERM_DATA_SERVICE_DELETED_GPS_ENTRIES_COUNT,
+                        -987);
+                onDataTransferredToLongTerm(totalRows, deletedAccelRows, deletedGpsRows);
+            }
+        };
+
+        tripListReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String tripsJsonString = intent.getStringExtra(
+                        ServiceConstants.TRIP_JSON_ARRAY_STRING);
+                try {
+                    JSONArray tripsJson = new JSONArray(tripsJsonString);
+                    onTripListReceived(tripsJson);
+                } catch (JSONException e) {
+                    Log.d(TAG, "onReceive: JSON Array Error parsing string.");
+                    e.printStackTrace();
+                }
             }
         };
 
@@ -38,10 +67,15 @@ public class BroadcastManager {
         IntentFilter longTermDataFilter = new IntentFilter(ServiceConstants.PROCESS_LONG_TERM_STORAGE);
         longTermDataFilter.addCategory(Intent.CATEGORY_DEFAULT);
         this.context.registerReceiver(longTermDataReceiver, longTermDataFilter);
+
+        IntentFilter tripListDataFilter = new IntentFilter(ServiceConstants.PROCESS_GET_ALL_TRIPS);
+        tripListDataFilter.addCategory(Intent.CATEGORY_DEFAULT);
+        this.context.registerReceiver(tripListReceiver, tripListDataFilter);
     }
 
     public void unregister() {
         this.context.unregisterReceiver(longTermDataReceiver);
+        this.context.unregisterReceiver(tripListReceiver);
     }
 
     public void onResume() {
@@ -50,5 +84,20 @@ public class BroadcastManager {
 
     public void onPause() {
         unregister();
+    }
+
+    public abstract void onDataTransferredToLongTerm(int totalRows,
+                                                     int deletedAccelRows,
+                                                     int deletedGpsRows);
+
+    public abstract void onTripListReceived(JSONArray tripList);
+
+    public abstract void onTripDataReceived(JSONObject tripData);
+
+    public void askToUpdateTripList() {
+        Intent getTripsListIntent = new Intent(context, DatabaseService.class);
+        getTripsListIntent.putExtra(ServiceConstants.SERVICE_PROCESS_TAG, ServiceConstants.PROCESS_GET_ALL_TRIPS);
+        Log.d(TAG, "askToUpdateTripList: Getting all trips");
+        context.startService(getTripsListIntent);
     }
 }
