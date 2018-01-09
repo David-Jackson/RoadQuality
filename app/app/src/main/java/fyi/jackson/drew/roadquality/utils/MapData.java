ba2dc0bd51eeae0401b06368725e1c130ecff4a4
David Jackson <jack8679@kettering.edu>
2018-01-09 09:58:16 -0500

    Split map polyline into multiple lines with a random color
    Color will be set based on the quality of the road at that point

--- app/app/src/main/java/fyi/jackson/drew/roadquality/utils/MapData.java
+++ app/app/src/main/java/fyi/jackson/drew/roadquality/utils/MapData.java
@@ -1,10 +1,12 @@
 package fyi.jackson.drew.roadquality.utils;
 
+import android.graphics.Color;
 import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.PolylineOptions;
 
@@ -69,16 +71,28 @@ public class MapData implements OnMapReadyCallback {
         try {
             JSONArray coordinates =
                     tripData.getJSONObject("trip").getJSONArray("coordinates");
-            PolylineOptions polylineOptions = new PolylineOptions();
             LatLngBounds.Builder latLngBuilder = new LatLngBounds.Builder();
-            for (int i = 0; i < coordinates.length(); i++) {
-                JSONObject coord = coordinates.getJSONObject(i);
-                com.google.android.gms.maps.model.LatLng latLng =
-                        new com.google.android.gms.maps.model.LatLng(coord.getDouble("lat"), coord.getDouble("lng"));
-                polylineOptions.add(latLng);
-                latLngBuilder.include(latLng);
+
+            float[] hsv = {0, 1, 1};
+            Color color = new Color();
+
+            for (int i = 0; i < coordinates.length() - 1; i++) {
+                PolylineOptions polylineOptions = new PolylineOptions();
+                JSONObject coord1 = coordinates.getJSONObject(i);
+                LatLng latLng1 =
+                        new LatLng(coord1.getDouble("lat"), coord1.getDouble("lng"));
+                polylineOptions.add(latLng1);
+                JSONObject coord2 = coordinates.getJSONObject(i + 1);
+                LatLng latLng2 =
+                        new LatLng(coord2.getDouble("lat"), coord2.getDouble("lng"));
+                polylineOptions.add(latLng2);
+
+                hsv[0] = i % 360;
+                polylineOptions.color(color.HSVToColor(hsv));
+
+                latLngBuilder.include(latLng1);
+                googleMap.addPolyline(polylineOptions);
             }
-            googleMap.addPolyline(polylineOptions);
             googleMap.animateCamera(CameraUpdateFactory.newLatLngBounds(latLngBuilder.build(), mapWidth, mapHeight, 10));
             isShowingData = true;
         } catch (JSONException e) {
