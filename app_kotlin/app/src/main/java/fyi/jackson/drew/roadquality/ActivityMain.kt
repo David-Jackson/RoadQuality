cc38561be4f52dddffda00c39aad1676bfe0d924
David Jackson <jack8679@kettering.edu>
2018-01-17 15:19:50 -0500

    Added res from Java app

--- app_kotlin/app/src/main/java/fyi/jackson/drew/roadquality/ActivityMain.kt
+++ app_kotlin/app/src/main/java/fyi/jackson/drew/roadquality/ActivityMain.kt
@@ -2,9 +2,10 @@ package fyi.jackson.drew.roadquality
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.widget.Toast
 
 class ActivityMain : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
